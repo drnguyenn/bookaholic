266b4374fcf9184f78c8bce630cbf134345eda64
NGUYEN DICH LONG <longnguyen25111998@gmail.com>
2020-04-06 16:25:03 +0700

    Updates

---
+++ precache-manifest.50c8fadbf7f7fec03a37d7590736b9be.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0ee47050548ff384cfbdd8fdbe149d3d",
+    "url": "/bookaholic/index.html"
+  },
+  {
+    "revision": "9f8857f64d2149075de2",
+    "url": "/bookaholic/static/css/2.de424728.chunk.css"
+  },
+  {
+    "revision": "1dad08f8e4054c00db55",
+    "url": "/bookaholic/static/css/main.f18f3a29.chunk.css"
+  },
+  {
+    "revision": "9f8857f64d2149075de2",
+    "url": "/bookaholic/static/js/2.120b9595.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "/bookaholic/static/js/2.120b9595.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1dad08f8e4054c00db55",
+    "url": "/bookaholic/static/js/main.a7bc228a.chunk.js"
+  },
+  {
+    "revision": "f4973732fbea60683881",
+    "url": "/bookaholic/static/js/runtime-main.b0e7e800.js"
+  },
+  {
+    "revision": "ee0c537500c5d346b9745afef5edba0b",
+    "url": "/bookaholic/static/media/bookaholic.ee0c5375.png"
+  }
+]);
